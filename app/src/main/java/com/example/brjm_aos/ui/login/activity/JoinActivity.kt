42f5f3d6ce3524cec659026faccd3fc28a203390
kimsiyeon <sxxyxx127@gmail.com>
2022-08-16 15:49:38 +0900

    #3 design : 회원가입 ui

---
+++ app/src/main/java/com/example/brjm_aos/ui/login/activity/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.brjm_aos.ui.login.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brjm_aos.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
