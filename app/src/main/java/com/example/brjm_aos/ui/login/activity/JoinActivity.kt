9ca79249c51eeadfe78aa2e4339afe19a8b996c2
kimsiyeon <sxxyxx127@gmail.com>
2022-08-17 23:07:04 +0900

    feat : button select 버튼이벤트추가 (#3)

--- app/src/main/java/com/example/brjm_aos/ui/login/activity/JoinActivity.kt
+++ app/src/main/java/com/example/brjm_aos/ui/login/activity/JoinActivity.kt
@@ -1,12 +1,26 @@
 package com.example.brjm_aos.ui.login.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.brjm_aos.R
+
+import com.example.brjm_aos.databinding.ActivityJoinBinding
+import com.example.brjm_aos.databinding.ActivityStartBinding
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        val binding = ActivityJoinBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.join2Button.setOnClickListener {
+            val intent = Intent(this, Join2Activity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/brjm_aos/ui/login/activity/StartActivity.kt
+++ app/src/main/java/com/example/brjm_aos/ui/login/activity/StartActivity.kt
@@ -2,11 +2,28 @@ package com.example.brjm_aos.ui.login.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.brjm_aos.R
+import android.content.Intent
+import com.example.brjm_aos.databinding.ActivityStartBinding
+
+
 
 class StartActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+
+        val binding = ActivityStartBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.startrgButton.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.startloginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
